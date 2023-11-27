58173fce86f07b326d86278b140aaa122885d6ab
DicarProjects <Cardona.DevProjects@gmail.com>
2023-11-26 21:48:46 -0500

    [Deploy Test] - disabled validator

--- src/validators/emailValidator.ts
+++ src/validators/emailValidator.ts
@@ -1,5 +1,5 @@
-import validator from 'validator';
+// import validator from 'validator';
 
-export function isValidEmail(email: string): boolean {
-  return validator.isEmail(email);
-}
+// export function isValidEmail(email: string): boolean {
+//   return validator.isEmail(email);
+// }
