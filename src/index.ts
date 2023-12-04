98f2c39af178d76c6269c2eb8c37daba32d31b17
DicarProjects <cardona.devprojects@gmail.com>
2023-12-04 07:10:59 -0500

    Fix (API): Add CORS and support for types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 const PORT = 3000;
 
 // Use the routes defined in the routes/index.ts file under the prefix /api
+app.use(cors());
 app.use('/api', routes);
 
 app.listen(PORT, () => {
