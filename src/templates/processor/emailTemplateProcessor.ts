9f4f4205d2af4ac28178ec87a1b99e3985c2fae8
DicarProjects <Cardona.DevProjects@gmail.com>
2023-12-06 10:23:41 -0500

    Add templatePath validation

--- src/templates/processor/emailTemplateProcessor.ts
+++ src/templates/processor/emailTemplateProcessor.ts
@@ -1,12 +1,17 @@
 import fs from 'fs';
 import { FormData } from '../../interfaces/FormData';
 import path from 'path';
+
 type FormKeys = keyof FormData;
 
 export function processEmailTemplate(formData: FormData): string {
   try {
-    const templatePath = path.join(__dirname,process.env.EMAIL_TEMPLATE_PATH!);
-    console.log(templatePath);
+    const templatePath = path.resolve(__dirname, process.env.EMAIL_TEMPLATE_PATH!);
+
+    if (!fs.existsSync(templatePath)) {
+      throw new Error("Email template not found");
+    }
+
     const htmlContent = fs.readFileSync(templatePath, 'utf8');
 
     const replacedHtml = htmlContent.replace(/\[(.+?)\]/g, (match, p1) => {
